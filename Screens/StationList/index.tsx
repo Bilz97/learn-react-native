23cac3de1de186a24fa30c7cbfc1963d8d4d9a7c
swathigasbuddy <sponnada@gasbuddy.com>
2022-02-16 23:14:13 +0530

    initial commit

---
+++ Screens/StationList/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function StationListScreen() {
+  return <Text>StationList</Text>;
+}
---
+++ Screens/Win/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function WinScreen() {
+  return <Text>Win Screen</Text>;
+}
