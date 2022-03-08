aa4e57c3bff91034935058701d455a23fb4d9537
Rafael <rlellys@gasbuddy.com>
2022-03-08 14:21:35 -0800

    Remove unused code to test commit;

--- Screens/Home/index.tsx
+++ Screens/Home/index.tsx
@@ -23,20 +23,6 @@ export function HomeScreen() {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  // const DATA = [
-  //   {
-  //     title: "charger 1",
-  //     data: ["address 1", "address 2", "address 3"]
-  //   },
-  //   {
-  //     title: "Charger 2",
-  //     data: ["address 1", "address 2", "address 3"]
-  //   },
-  //   {
-  //     title: "Charger 3",
-  //     data: ["address 1", "address 2", "address 3"]
-  //   },
-  // ];
   return (
     <SafeAreaView style={backgroundStyle}>
       <ScrollView
@@ -47,22 +33,3 @@ export function HomeScreen() {
     </SafeAreaView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
